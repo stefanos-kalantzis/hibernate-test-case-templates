4c21b8e5bff8ef21c2c06aa489a8fdaf925d5cc2
Stefanos Kalantzis <steve.tcmg@gmail.com>
2023-09-07 13:51:27 +0200

    HHH-17180: implement test that reproduces reported issue

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,38 +1,80 @@
 package org.hibernate.bugs;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Persistence;
-
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.testcontainers.containers.MariaDBContainer;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
  */
 public class JPAUnitTestCase {
 
-	private EntityManagerFactory entityManagerFactory;
+    private MariaDBContainer mariaDBContainer = new MariaDBContainer<>("mariadb:10.5.21")
+            .withUsername("mariadb1")
+            .withPassword("mariadb1")
+            .withDatabaseName("db1")
+            .withInitScript("init-script.sql");
 
-	@Before
-	public void init() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-	}
+    private EntityManagerFactory entityManagerFactory;
 
-	@After
-	public void destroy() {
-		entityManagerFactory.close();
-	}
+    @Before
+    public void init() {
+        mariaDBContainer.setPortBindings(List.of("3316:3306"));
+        mariaDBContainer.start();
+        entityManagerFactory = Persistence.createEntityManagerFactory("templatePU");
+    }
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
-	@Test
-	public void hhh123Test() throws Exception {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		// Do stuff...
-		entityManager.getTransaction().commit();
-		entityManager.close();
-	}
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+        mariaDBContainer.stop();
+    }
+
+    @Test
+    public void hhh17180Test() throws Exception {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(new Animal(AnimalType.CAT, "Fluffies"));
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+
+    @Entity(name = "animal")
+    public static class Animal {
+
+        @Id
+        @GeneratedValue
+        private Integer id;
+
+        // @JdbcTypeCode(SqlTypes.VARCHAR)
+        @Enumerated(EnumType.STRING)
+        private AnimalType animalType;
+
+        private String name;
+
+        public Animal() {
+        }
+
+        public Animal(AnimalType animalType, String name) {
+            this.animalType = animalType;
+            this.name = name;
+        }
+    }
+
+    public enum AnimalType {
+        CAT, DOG
+    }
 }
---
+++ orm/hibernate-orm-6/src/test/resources/init-script.sql
@@ -0,0 +1,5 @@
+create table animal (
+    id integer not null primary key,
+    animalType varchar(255) null,
+    name varchar(255)
+);
